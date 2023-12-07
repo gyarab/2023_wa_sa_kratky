3410db3c2e313f2a3b0b2e2431dbb4e97270c953
MartinKratkyGA <94782113+MartinKratkyGA@users.noreply.github.com>
2023-12-07 17:54:50 +0100

    Update pexeso.js

--- pexeso.js
+++ pexeso.js
@@ -1,16 +1,38 @@
-var otaznik = false;
+var numofturned = 0;
+var elements = 8*2
 
 function turnCard(cardid) {
-    otaznik = !otaznik
-    console.log(otaznik)
+    
+    numofturned = numofturned + 1;
 
-    const el = document.getElementById("card-" + cardid)
-    if (otaznik) {
-        el.innerHTML = '<img src="img/pepe' + cardid +'.jfif" alt="pepecard">';
+    if(numofturned > 2){
+        for(var a = 1; a < elements; a++){
+            otaznik(a)
+        }
+        numofturned = 1;
     }
-    else {
-        el.innerHTML = '<img src="img/otaznik.png" alt="pepecard">';
+
+    const el = document.getElementById("card-" + cardid);
+    obrazek(el, cardid);
+}
+
+function obrazek(el, cardid) {
+    if(cardid > 8){
+        cardid = cardid - 8
+    }
+    if(cardid < 3){
+        el.src ='img/obr' + cardid +'.jfif';
+    } else {
+        el.src = 'img/obr' + cardid +'.jpg';
     }
 }
 
-let el = document.getElementById('stav-hry')
+function otaznik(a) {
+
+
+    const el = document.getElementById("card-" + a);
+
+    el.src = 'img/otaznik.png';
+}
+
+let elmt = document.getElementById('stav-hry');
