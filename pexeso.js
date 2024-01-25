a3baedd992666e95079ae3f7e225a9e7c33e4854
MartinKratkyGA <94782113+MartinKratkyGA@users.noreply.github.com>
2024-01-25 17:25:18 +0100

    Update pexeso.js

--- pexeso.js
+++ pexeso.js
@@ -1,18 +1,33 @@
 var numofturned = 0;
+var lastturned = 0;
 var elements = 8*2;
+var cards = [0, 0, 0, 0, 0, 0, 0, 0];
 
 function turnCard(cardid) {
     
-    numofturned = numofturned + 1;
+    if (cardid != lastturned){
 
-    if(numofturned > 2){
-
-        for(var a = 1; a <= elements; a++){
-            questionmark(a); 
+        
+        console.log(cardid%8 == lastturned%8);
+        if (cardid%8 == lastturned%8){
+            cards[(cardid-1)%8] = 1;
         }
-        numofturned = 1;
+
+        lastturned = cardid;
+
+        numofturned = numofturned + 1;
+
+        if(numofturned > 2){
+    
+            for(var a = 1; a <= elements; a++){
+                questionmark(a); 
+            }
+            numofturned = 1;
+        }
+        picture(cardid);
+
     }
-    picture(cardid);
+
 }
 
 function picture(cardid) {
@@ -20,7 +35,7 @@ function picture(cardid) {
     const el = document.getElementById("card-" + cardid);
 
     if(cardid > 8){
-        cardid = cardid - 8
+        cardid = cardid - 8;
     }
     if(cardid < 3){
         el.src ='img/obr' + cardid +'.jfif';
@@ -34,6 +49,13 @@ function questionmark(a) {
     const el = document.getElementById("card-" + a);
 
     el.src = 'img/otaznik.png';
+
+    for(var b = 0; b < 9; b++){
+        if(cards[b] == 1){
+            picture(b + 1);
+            picture(b+8 + 1);
+        }
+    }
 }
 
 let elmt = document.getElementById('stav-hry');
